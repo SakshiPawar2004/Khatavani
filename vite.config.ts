bf2c0947a41d3e810ee991021372ffee9c32e0ac
SakshiPawar2004 <sdpawar370122@kkwagh.edu.in>
2025-07-20 08:11:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['ledgerbook-zl4s.onrender.com']
+  }
 });
